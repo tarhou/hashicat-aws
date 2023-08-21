24eefb09663db9033e8e311fae89a9d47229390e
tarhou <77625278+tarhou@users.noreply.github.com>
2023-08-21 11:52:46 +0200

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/instruqt-lab-org/s3-bucket/aws"
+  version = "2.8.0"
 }
